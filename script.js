7aa303d56c3ee9fb4ef800eb2635c52c743f1c65
Lil JSF <90755554+KRBNJSF@users.noreply.github.com>
2023-02-13 11:23:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,35 @@ box.id = "box";
 box.classList.add("box");
 document.body.appendChild(box);
 
-box.innerText = weekNumber % 2 == 0 ? "Po, St, Pá, Ne" : "Út, Čt, So";
+let today = new Date().getDay();
+
+let objDay = [
+    ["Po", 1],
+    ["Ut", 2],
+    ["St", 3],
+    ["Ct", 4],
+    ["Pa", 5],
+    ["So", 6],
+    ["Ne", 7]
+];
+
+let result = '';
+
+for (let i = 0; i < objDay.length; i++) {
+    if (i % 2 != 0) {
+        if (today == objDay[i][1]) {
+            result = result + "<span class='colar'>" + objDay[i][0] + "</span> "
+        }
+        else {
+            result = result + " " + objDay[i][0]
+        }
+    }
+}
+
+
+
+// box.innerText = weekNumber % 2 == 0 ? "Po, St, Pá, Ne" : "Út, Čt, So";
+box.innerHTML = result;
 
 console.log("Week number of " + currentDate +
     " is : " + weekNumber);
